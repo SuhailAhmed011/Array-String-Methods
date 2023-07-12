5f26e34f7fa51ba3c0c091b545c900ade168d806
Suhail Ahmed <127311098+SuhailAhmed011@users.noreply.github.com>
2023-07-12 23:07:16 +0530

    Add files via upload

---
+++ array-methods.js
@@ -0,0 +1,68 @@
+//For each method
+
+/*let names = ['suhail','sameer', 'faizan', 'Abbu'];
+names.forEach((items, index) =>{
+    console.log(index + " " + ':' + items );
+}); 
+
+// sort method
+
+let names = ['suhail','sameer', 'faizan', 'Abbu'];
+names.sort();
+console.log(names);
+
+// reverse method
+
+let name = ['suhail','sameer', 'faizan', 'Abbu'];
+name.reverse();
+console.log(name); 
+
+//indexOf method
+let names = ['suhail', 'saim', 'sarfaraz', 'saim','shoaib'];
+let a = names.indexOf('saim', 2)
+console.log(a); 
+
+//includes method
+let names = ['suhail', 'saim', 'sarfaraz', 'saim','shoaib'];
+let b = names.includes('saim');
+console.log(b); 
+
+// some and every method
+let ages = [12,14,17,11];
+let b = ages.some(checkAdult); // for every method it is same as some we have to change just place every at the place of some.
+console.log(b);
+
+function checkAdult(age){
+    return age >= 18;
+}  
+
+// filter method
+let ages = [12,19,17,28];
+let b = ages.filter(adultAges);
+console.log(b);
+function adultAges(age){
+ return age>=18;
+} 
+
+// map method
+let numbers = [
+    {fname : 'suhail', lname : 'ahmed'},
+    {fname : 'ali', lname : 'hamza'},
+    {fname : 'saif', lname : 'khan'}
+];
+let b = numbers.map((x) =>{
+    return x.fname + " " + x.lname;
+    
+})
+console.log(b); 
+
+// slice method
+
+let names = ['suhail', 'zain', 'malik', 'ali'];
+let b = names.slice(1, 3);
+console.log(b); // array mai se kisi value ko alag nikalni ho to we use slice 
+
+// splice method
+let names = ['suhail', 'zain', 'malik', 'ali'];  
+names.splice(1,0, 'haider', 'usman');
+console.log(names); */
